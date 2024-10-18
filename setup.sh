0c1dcbd1d11b01712b49341c4c294cd5671db495
Rafał Majewski <goodheropl@gmail.com>
2024-10-18 14:40:20 +0200

    feature: Add installing Visual Studio Code to the setup script

--- setup.sh
+++ setup.sh
@@ -63,3 +63,6 @@ cp $(dirname $0)/assets/.config/i3/scripts/decrease-brightness.sh ~/.config/i3/s
 cp $(dirname $0)/assets/.config/i3/scripts/increase-brightness.sh ~/.config/i3/scripts/increase-brightness.sh
 cp $(dirname $0)/assets/.xinitrc ~/.xinitrc
 i3-msg reload
+
+echo "Installing Visual Studio Code..."
+yay --sync --needed visual-studio-code-bin

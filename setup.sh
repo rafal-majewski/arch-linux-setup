9232e38a1add7362d40f4fd039be392d990ae7aa
Rafał Majewski <goodheropl@gmail.com>
2024-10-18 14:40:20 +0200

    feature: Add installing Visual Studio Code to the setup script

--- setup.sh
+++ setup.sh
@@ -56,4 +56,6 @@ echo "Configuring i3..."
 rsync --archive --delete "$(dirname "${0}")/assets/.config/i3/" "${HOME}/.config/i3/"
 i3-msg reload
 echo "Configuring xinit..."
-cp "$(dirname "${0}")/assets/.xinitrc" "${HOME}/.xinitrc"
+cp "$(dirname "${0}")/assets/.xinitrc" "$HOME/.xinitrc"
+echo "Installing Visual Studio Code..."
+yay --sync --needed visual-studio-code-bin

383c03350bd2e2acf6f6b4c5efa3370f42a65bb3
Rafał Majewski <goodheropl@gmail.com>
2024-10-18 13:42:06 +0200

    feature: Add setting the git user name

--- setup.sh
+++ setup.sh
@@ -2,3 +2,10 @@
 
 echo "Updating what's currently installed using Pacman..."
 sudo pacman --sync --refresh --sysupgrade
+
+echo "Checking if the git user name is set..."
+if [ -z "$(git config --global user.name)" ]; then
+	read -p "Enter your git user name: " GIT_USER_NAME
+	echo "Setting the git user name..."
+	git config --global user.name "$GIT_USER_NAME"
+fi

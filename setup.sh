d58092971816b38a34488b13dd51366f3d5d7912
Rafał Majewski <goodheropl@gmail.com>
2024-10-25 15:14:49 +0200

    feature: Add installing Spectacle to the setup script

--- setup.sh
+++ setup.sh
@@ -84,3 +84,5 @@ cp "$(dirname "${0}")/assets/.bash_profile" "${HOME}/.bash_profile"
 cp "$(dirname "${0}")/assets/.bash_logout" "${HOME}/.bash_logout"
 echo "Installing the \"noto-fonts-emoji\" package..."
 yay --sync --needed noto-fonts-emoji
+echo "Installing Spectacle"
+yay --sync --needed spectacle

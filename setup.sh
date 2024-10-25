ad97e20d42253ba2e3c6b9e151fe3d97a2a2dece
Rafał Majewski <goodheropl@gmail.com>
2024-10-25 14:48:56 +0200

    feature: Add setting the Git user name to the setup script

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@ echo "Updating what is currently installed using Pacman..."
 sudo pacman --sync --refresh --sysupgrade
 echo "Installing Git..."
 sudo pacman --sync --needed git
+echo "Checking if the Git user name is set..."
+if [ -z "$(git config --global user.name)" ]; then
+	read -p "Enter your Git user name: " GIT_USER_NAME
+	echo "Setting the Git user name..."
+	git config --global user.name "${GIT_USER_NAME}"
+fi

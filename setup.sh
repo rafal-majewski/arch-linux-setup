5b0568f6beaea0a5d8e938ead80a4bde2cebbb11
Rafał Majewski <goodheropl@gmail.com>
2024-11-10 17:53:02 +0100

    feature: Add starting the Greenclip service to the setup script

--- setup.sh
+++ setup.sh
@@ -87,3 +87,5 @@ echo "Configuring Greenclip..."
 cp "$(dirname $0)/assets/.config/greenclip.toml" "$HOME/.config/greenclip.toml"
 echo "Enabling the Greenclip service..."
 systemctl --user enable greenclip
+echo "Starting the Greenclip service..."
+systemctl --user start greenclip

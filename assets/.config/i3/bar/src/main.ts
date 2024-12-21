580c1395ccf5bf8c5495816476fc675b81a07f37
Rafał Majewski <goodheropl@gmail.com>
2024-12-21 21:10:46 +0100

    feature: Add displaying the volume status to the bar

--- assets/.config/i3/bar/src/main.ts
+++ assets/.config/i3/bar/src/main.ts
@@ -2,6 +2,8 @@
 import {MessageSegmentProvider} from "./core/message-segment-provider/MessageSegmentProvider.ts";
 import {run} from "./core/running/run.ts";
 import {TimestampMessageSegmentComputer} from "./message-segment-computer-implementations/timestamp/TimestampMessageSegmentComputer.ts";
+import {VolumeStatusMessageSegmentComputer} from "./message-segment-computer-implementations/volume-status/VolumeStatusMessageSegmentComputer.ts";
 await run([
+	new MessageSegmentProvider(new VolumeStatusMessageSegmentComputer(), 1),
 	new MessageSegmentProvider(new TimestampMessageSegmentComputer(), 1),
 ]);

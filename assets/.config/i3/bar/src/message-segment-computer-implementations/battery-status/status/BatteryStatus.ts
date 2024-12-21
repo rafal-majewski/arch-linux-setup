b879fed7453078d7cd96f2639e6ea258feb8fa45
Rafał Majewski <goodheropl@gmail.com>
2024-12-21 21:10:24 +0100

    feature: Add displaying the battery status to the bar

---
+++ assets/.config/i3/bar/src/message-segment-computer-implementations/battery-status/status/BatteryStatus.ts
@@ -0,0 +1,5 @@
+import type {BatteryStatusChargingState} from "./charging-state/BatteryStatusChargingState.ts";
+export type BatteryStatus = Readonly<{
+	chargingState: BatteryStatusChargingState;
+	level: number;
+}>;

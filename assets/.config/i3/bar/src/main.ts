464f8f5bbd2fced816eb41ecaabca6f911cd78e8
Rafał Majewski <goodheropl@gmail.com>
2024-12-21 21:10:24 +0100

    feature: Add displaying the battery status to the bar

--- assets/.config/i3/bar/src/main.ts
+++ assets/.config/i3/bar/src/main.ts
@@ -1,11 +1,16 @@
 #!/usr/bin/env -S deno run --allow-run
 import {run} from "./core/running/run.ts";
+import {BatteryStatusMessageSegmentComputer} from "./message-segment-computer-implementations/battery-status/BatteryStatusMessageSegmentComputer.ts";
 import {TimestampMessageSegmentComputer} from "./message-segment-computer-implementations/timestamp/TimestampMessageSegmentComputer.ts";
 import {VolumeStatusMessageSegmentComputer} from "./message-segment-computer-implementations/volume-status/VolumeStatusMessageSegmentComputer.ts";
+const batteryStatusMessageSegmentComputer =
+	new BatteryStatusMessageSegmentComputer();
 const volumeStatusMessageSegmentComputer =
 	new VolumeStatusMessageSegmentComputer();
+new VolumeStatusMessageSegmentComputer();
 const timestampMessageSegmentComputer = new TimestampMessageSegmentComputer();
 const messageSegmentComputers = [
+	batteryStatusMessageSegmentComputer,
 	volumeStatusMessageSegmentComputer,
 	timestampMessageSegmentComputer,
 ] as const;

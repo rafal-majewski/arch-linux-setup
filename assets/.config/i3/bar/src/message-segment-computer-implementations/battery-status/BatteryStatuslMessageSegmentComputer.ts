f40b2e9d07607164471f4e3cab39ffe6ef46cefd
Rafał Majewski <goodheropl@gmail.com>
2024-12-21 21:10:15 +0100

    feature: Add displaying the timestamp to the bar

---
+++ assets/.config/i3/bar/src/message-segment-computer-implementations/battery-status/BatteryStatuslMessageSegmentComputer.ts
@@ -0,0 +1,27 @@
+import {AsynchronousMessageSegmentComputer} from "../../core/message-segment-computer/types/asynchronous/AsynchronousMessageSegmentComputer.ts";
+import {computeBatteryStatus} from "./computing-status/computeBatteryStatus.ts";
+import {formatBatteryStatus} from "./formatting/formatBatteryStatus.ts";
+export class BatteryStatusMessageSegmentComputer extends AsynchronousMessageSegmentComputer {
+	public override async compute(): Promise<string> {
+		const decoder = new TextDecoder();
+		const [capacityCommandOutput, statusCommandOutput] = (
+			await Promise.all([
+				new Deno.Command("cat", {
+					args: ["/sys/class/power_supply/BAT1/capacity"],
+					stdout: "piped",
+				}).output(),
+				new Deno.Command("cat", {
+					args: ["/sys/class/power_supply/BAT1/status"],
+					stdout: "piped",
+				}).output(),
+			])
+		).map(({stdout}) => decoder.decode(stdout));
+		const status = computeBatteryStatus(
+			capacityCommandOutput,
+			statusCommandOutput,
+		);
+		const formattedStatus = formatBatteryStatus(status);
+		return formattedStatus;
+	}
+	public override readonly intervalSeconds = 8;
+}
---
+++ assets/.config/i3/bar/src/message-segment-computer-implementations/volume-status/VolumeStatusMessageSegmentComputer.ts
@@ -0,0 +1,18 @@
+import {AsynchronousMessageSegmentComputer} from "../../core/message-segment-computer/types/asynchronous/AsynchronousMessageSegmentComputer.ts";
+import {computeVolumeStatus} from "./computing-status/computeVolumeStatus.ts";
+import {formatVolumeStatus} from "./formatting/formatVolumeStatus.ts";
+export class VolumeStatusMessageSegmentComputer extends AsynchronousMessageSegmentComputer {
+	public override async compute(): Promise<string> {
+		const command = new Deno.Command("wpctl", {
+			args: ["get-volume", "@DEFAULT_AUDIO_SINK@"],
+			stdout: "piped",
+		});
+		const commandOutput = new TextDecoder().decode(
+			(await command.output()).stdout,
+		);
+		const status = computeVolumeStatus(commandOutput);
+		const formattedStatus = formatVolumeStatus(status);
+		return formattedStatus;
+	}
+	public override readonly intervalSeconds = 1;
+}

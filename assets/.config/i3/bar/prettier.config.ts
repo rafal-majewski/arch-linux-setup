f36b6b2c76704221fce64be1e10ee65ae2fa13d3
Rafał Majewski <goodheropl@gmail.com>
2025-03-20 21:29:10 +0100

    chore: Add prettier cong

---
+++ assets/.config/i3/bar/prettier.config.ts
@@ -0,0 +1,15 @@
+export default {
+	arrowParens: "always",
+	bracketSpacing: false,
+	endOfLine: "lf",
+	experimentalOperatorPosition: "start",
+	experimentalTernaries: true,
+	objectWrap: "collapse",
+	plugins: [],
+	printWidth: 80,
+	quoteProps: "as-needed",
+	semi: true,
+	singleQuote: false,
+	trailingComma: "all",
+	useTabs: true,
+};
